e3b22e9cf0d83c4a3e4293fdab87c8c5dcaa1c14
DESKTOP-L715VDK\duke <ychen18nj@gmail.com>
2017-12-15 17:26:44 +0800

    fix import err in execute

--- execute.cpp
+++ execute.cpp
@@ -1,41 +1,54 @@
 #include "Python.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	// declare python objects
-	PyObject *pName, *pModule, *pDict, *pFunc, *pValue;
+	PyObject *pModule, *pDict, *pFunc, *pValue;
 
-	// usage: exe_name, python_src, func_name
-	if(argc == 3)
+	// initialize python interpretor
+	Py_Initialize();
+
+	// build python name project
+	// this is no longer available in python 3
+	// pName = PyString_FromString(argv[1]);
+	//pName = PyBytes_FromString("plasma");
+
+	// load python module object
+	pModule = PyImport_ImportModule("plasma.entry");
+
+	if(pModule == 0)
 	{
-		// initialize python interpretor
-		Py_initialize();
-
-		// build python name project
-		pName = PyString_FromString(argv[1]);
-
-		// load python module object
-		pModule = PyImport_Import(pName);
-
-		// get dict borrowed reference from module
-		pDict = PyModule_GetDict(pModule);
-
-		// get func borrowed reference from dict
-		pFunc = PyDict_GetItemString(pDict, argv[2]);
-
-		if(PyCallable_Check(pFunc))
-			// if func is callable, call python function
-			PyObject_CallObject(pFunc, NULL);
-		else
-			PyErr_Print();
-
-		// no need to decref borrowed references
-		Py_DECREF(pModule);
-		Py_DECREF(pName);
-
-		// call finalize function
-		Py_Finalize();
-
-		return 0;
+		PyErr_Print();
+		printf("Could not load python module: plasma.entry\n");
 	}
+	else
+		printf("Found python module: plasma\n");
+
+	// get dict borrowed reference from module
+	pDict = PyModule_GetDict(pModule);
+
+	// get func borrowed reference from dict
+	pFunc = PyDict_GetItemString(pDict, "test");
+
+	if(PyCallable_Check(pFunc))
+	{
+		// if func is callable, call python function
+		printf("Found python callable function: plasma.entry.test\n");
+		pValue = PyObject_CallObject(pFunc, NULL);
+	}
+	else
+	{
+		printf("Python function: plasma.entry.test is not callable\n");
+		PyErr_Print();
+	}
+
+	// no need to decref borrowed references
+	Py_DECREF(pModule);
+	//Py_DECREF(pName);
+	Py_DECREF(pValue);
+	// call finalize function
+	Py_Finalize();
+
+	return 0;
 }
--- start.py
+++ start.py
@@ -1,7 +1,8 @@
-import plasma
+import plasma.entry
 
 def main():
 	print ("hello plazma")
+	test()
 	return
 
 if __name__ == "__main__":
